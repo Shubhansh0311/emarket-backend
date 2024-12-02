b243c170207f60add76d38a2fee54dd4f5124f44
shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-12-03 00:05:51 +0530

    test3

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Enable CORS with options
 app.use(express.json()); // Use the built-in express.json() to parse JSON requests
 app.use("/", router); // Register your routes
-app.get("/api",(req,res)=>{
+app.get("/api/getproducts/:id",(req,res)=>{
   res.json({message:'route working'})
 })
 // Port from environment or default to 800
