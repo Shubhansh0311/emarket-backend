509027f0bed8528f86e6512c81878360050e9b79
shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-12-02 23:27:41 +0530

    cors policy updated

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ connectDB()
 app.use(bodyParser.json());
 const port=process.env.PORT||800
 // app.use(cors())
-app.use(cors({
-    // origin: 'http://localhost:3000'  // Replace with the actual URL of your frontend
-    origin: 'https://emarket-frontend.vercel.app' , // Replace with the actual URL of your frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include OPTIONS
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-    credentials: true, // Include cookies in requests if needed
-  }));
+const corsOptions = {
+  origin: 'https://setting-app-frontend.vercel.app', // Your React app URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Include OPTIONS
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true, // Include cookies in requests if needed
+};
+
+app.options('*', cors(corsOptions)); // Handle preflight requests
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use("/",router)
 app.listen(port,()=>{
     console.log("Server is listening at http://localhost:",port);
