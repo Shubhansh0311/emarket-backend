cfaf336bcfda60ab9ba962d9e7383e22d4834434
shubhansh0311 <shubhanshagrawal0311@gmail.com>
2024-12-03 22:42:31 +0530

    test103

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
  import dotenv from "dotenv"
 const connectDB=()=>{
+    MONGODB_URL="mongodb+srv://shubhanshagrawal0311:shubhansh0311@cluster0.fhq4k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
     dotenv.config()
-    const db=mongoose.connect(process.env.MONGODB_URL).then((e)=>{
+    const db=mongoose.connect(MONGODB_URL).then((e)=>{
         console.log("database connected succesfully");
         
     }).catch((err)=>{
